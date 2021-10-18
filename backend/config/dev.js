b5b01237019ffe110bfbc9893796cfd04b0ab0f9
doluwamu <dboyadeitan@gmail.com>
2021-10-18 19:03:03 +0100

    fixed errors

--- backend/config/dev.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  DB_URI:
-    "mongodb+srv://doluwamu:proshopeeee@cluster0.q6szi.mongodb.net/proshope-dev?retryWrites=true&w=majority",
-  JWT_SECRET: "grudiu8weu4ui8949iurii",
-};
-
-export default config;
